a9938cfd5bb17335670c6d0effd5c5f5e0ce3aa3
Sasithon Dontree <sasithon.dontree@mail.kmutt.ac.th>
2022-10-26 05:14:11 +0700

    intro-js

---
+++ intro-js-G1/main.js
@@ -0,0 +1,10 @@
+console.log("Sasitohn Dontree")
+function displayMessage(msg){
+    console.log("starting...function")
+    console.log(msg)
+    console.log("ending...function")
+}
+// weakly type ไม่ผูกไทป์
+
+let greetingMsg ='welcome JavaScript Clss'
+displayMessage(greetingMsg)
