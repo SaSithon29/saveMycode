a6d1ad99d9cb2a5080f24783d971994649d1c42e
mmeeppoo <88130453+SaSithon29@users.noreply.github.com>
2022-12-13 19:41:41 +0700

    Delete M3 directory

--- M3/basic-module/myLib/bmain.js
+++
@@ -1,6 +0,0 @@
-import{BookMark} from './bookMarkManagement.js'
-const myBookMark = new BookMark('1-234-5678',250)
-console.log(myBookMark)
-
-// npm init สร้างแพคเกจ json
-// 
--- M3/basic-module/myLib/gmainn.js
+++
@@ -1,5 +0,0 @@
-const {getItemsOfCurrentPage, codeId, doSomething } = require('./getItemsOfCurrentPage.js')
-
-console.log(getItemsOfCurrentPage([1,2,3,4,5,87,9,21,41,32],1,5))
-console.log(codeId)
-console.log(doSomething())
